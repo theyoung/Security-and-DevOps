d7bdb5b855fedaeded0e408e352e7eca0222d7d5
stevenna <theyoung2002@naver.com>
2022-09-13 13:07:15 +0900

    - remove unnecessary annotation

--- starter_code/src/main/java/com/example/demo/security/JWTAuthenticationVerificationFilter.java
+++ starter_code/src/main/java/com/example/demo/security/JWTAuthenticationVerificationFilter.java
@@ -18,7 +18,6 @@ import java.util.ArrayList;
 
 import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 
-@Component
 public class JWTAuthenticationVerificationFilter extends BasicAuthenticationFilter {
 
     public JWTAuthenticationVerificationFilter(AuthenticationManager authManager) {
